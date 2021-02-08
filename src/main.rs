e40a28ea1723fc80618d6898ed0b85bd7d11a594
Brian Feaster <bfeaster@uplift.com>
2021-02-07 22:40:15 -0800

    Telegram plusy emoji enabled.

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use log::*;
 use std::time::{Duration};
 use std::collections::{HashMap, HashSet};
 use std::str::{from_utf8, Utf8Error};
+use std::env::{args, Args};
 use actix_web::{web, App, HttpRequest, HttpServer, HttpResponse, Route};
 use actix_web::client::{Client, Connector};
 use openssl::ssl::{SslConnector, SslAcceptor, SslFiletype, SslMethod};
@@ -10,14 +11,15 @@ use json::{JsonValue};
 use std::fs::{read_to_string, write};
 
 /*
+use ::serde::{Serialize, Deserialize};
+use ::serde_json::{Value, from_str, to_string_pretty};
 const  OFF :&str = "\x1b[0m";
 const  MAG :&str = "\x1b[36m";
 const  GRN :&str = "\x1b[32m";
 const BGRN :&str = "\x1b[1;32m";
+fn ginfo<T: std::fmt::Debug>(e: T) { info!("{:?}", e); }
+fn gerror<T: std::fmt::Debug>(e: T) { error!("{:?}", e); }
 */
-
-//use ::serde::{Serialize, Deserialize};
-//use ::serde_json::{Value, from_str, to_string_pretty};
 
 #[derive(Debug)]
 enum Serror {
@@ -47,7 +49,7 @@ impl From<log::SetLoggerError> for Serror {
     fn from(e: log::SetLoggerError) -> Self { Serror::SetLoggerError(e) }
 }
 
-async fn sendmsg (botkey :&str, chat_id: &str, text: &String) {
+async fn sendmsg (botkey :&str, chat_id: &str, text: &str) {
     let mut builder = SslConnector::builder(SslMethod::tls()).unwrap();
     builder.set_private_key_file("key.pem", openssl::ssl::SslFiletype::PEM).unwrap();
     builder.set_certificate_chain_file("cert.pem").unwrap();
@@ -63,6 +65,7 @@ async fn sendmsg (botkey :&str, chat_id: &str, text: &String) {
                 "/sendmessage" +
                 "?chat_id=" + chat_id +
                 "&text=" + text +
+                //"&parse_mode=HTML" +
                 "&disable_notification=true" )
         .header("User-Agent", "Actix-web")
         .timeout(Duration::new(10,0))
@@ -156,7 +159,7 @@ fn parse_tickers (txt :&str) -> HashSet<String> {
 }
 
 
-fn json_chat_id (json :&JsonValue, default_chat_id :&str) -> String {
+fn json_message_chat_id (json :&JsonValue, default_chat_id :&str) -> String {
     match json["message"]["chat"]["id"].as_number() {
         Some(num) => {
             let ns = num.to_string();
@@ -170,7 +173,7 @@ fn json_chat_id (json :&JsonValue, default_chat_id :&str) -> String {
 async fn do_ticker (botkey :&String, chat_id_default :&str, json :&JsonValue) {
 
     // Who gets response?
-    let chat_id = json_chat_id(json, chat_id_default);
+    let chat_id = json_message_chat_id(json, chat_id_default);
 
     let mut txt = &json["message"]["text"].as_str(); // might return null
     let qry = &json["inline_query"]["query"].as_str(); // might return null
@@ -195,34 +198,70 @@ async fn do_ticker (botkey :&String, chat_id_default :&str, json :&JsonValue) {
 }
 
 async fn do_plussy_all (botkey :&String, chat_id_default :&str, json :&JsonValue) {
-    let chat_id = json_chat_id(json, chat_id_default);
+    let chat_id = json_message_chat_id(json, chat_id_default);
     let textfield = &json["message"]["text"];
     if textfield != "+?" {
         error!("plussy_all  txt {:?}", textfield);
         return;
     }
 
+    let mut likes = Vec::new();
+    // Over each user in file
     for l in read_to_string("telegram/users.txt").unwrap().lines() {
         let v = l.split(" ").collect::<Vec<&str>>();
         let id = v[0];
-        let nom = v[1];
+        let nom = v[1].to_string();
 
-        let likes = read_to_string( "telegram/".to_string() + &id )
-            .unwrap_or("0".to_string());
+        // Read the count file
+        let count = read_to_string( "telegram/".to_string() + &id )
+            .unwrap_or("0".to_string()).parse::<i32>().unwrap();
 
-        let text = format!("{}({})", nom, likes);
-        info!("{} {} -> msg telegram {:?}", id, text, sendmsg(botkey, &chat_id, &text).await);
+        likes.push((count, nom));
     }
+
+
+    let mut text = String::new();
+    likes.sort_by(|a,b| b.0.cmp(&a.0) );
+    // %3c %2f b %3e
+    for (likes,nom) in likes {
+        text.push_str(&format!("+{}{}", nom, num2heart(likes)));
+    }
+    //info!("HEARTS -> msg telegram {:?}", sendmsg(botkey, &chat_id, &(-11..=14).map( |n| num2heart(n) ).collect::<Vec<&str>>().join("")).await);
+    info!("{:?} -> msg telegram {:?}", text, sendmsg(botkey, &chat_id, &text[1..]).await);
+}
+
+fn num2heart (n :i32) -> &'static str {
+    if n < -4  { return "%CE%BB"; } // lambda
+    if n < 0  { return "%F0%9F%96%A4"; } // black heart
+    if n == 0 { return "%F0%9F%92%94"; } // red broken heart
+    if n == 1 { return "%E2%9D%A4%EF%B8%8F"; } // red heart
+    if n == 2 { return "%F0%9F%A7%A1"; } // orange heart
+    if n == 3 { return "%F0%9F%92%9B"; } // yellow heart
+    if n == 4 { return "%F0%9F%92%9A"; } // green heart
+    if n == 5 { return "%F0%9F%92%99"; } // blue heart
+    if n == 6 { return "%F0%9F%92%9C"; } // violet heart
+    if n == 7 { return "%F0%9F%92%97"; } // pink growing heart
+    if n == 8 { return "%F0%9F%92%96"; } // pink sparkling heart
+    if n == 9 { return "%F0%9F%92%93"; } // beating heart
+    if n == 10 { return "%F0%9F%92%98"; } // heart with arrow
+    if n == 11 { return "%F0%9F%92%9D"; } // heart with ribbon
+    return "%F0%9F%92%9E" // revolving hearts
 }
 
 async fn do_plussy (botkey :&String, chat_id_default :&str, json :&JsonValue) {
-    let chat_id = json_chat_id(json, chat_id_default);
-    let textfield = &json["message"]["text"];
+    let chat_id = json_message_chat_id(json, chat_id_default);
+    let textfield = &json["message"]["text"].as_str().unwrap_or("");
     let from = &json["message"]["from"]["id"].as_i64();
     let to = &json["message"]["reply_to_message"]["from"]["id"].as_i64();
 
-    if textfield != "+1" || from.is_none() || to.is_none() || from==to {
-        error!("plussy txt {:?}  from {:?}  to {:?}", textfield, from, to);
+    let amt :i32 = match textfield {
+        &"+1" => 1,
+        &"-1" => -1,
+        _ => 0
+    };
+
+    if amt == 0 || from.is_none() || to.is_none() || from==to {
+        error!("plussy txt {:?}  amt {:?}  from {:?}  to {:?}", textfield, amt, from, to);
         return;
     }
 
@@ -245,28 +284,28 @@ async fn do_plussy (botkey :&String, chat_id_default :&str, json :&JsonValue) {
 
     let flikes = read_to_string( "telegram/".to_string() + &from )
         .unwrap_or("0".to_string())
-        .parse::<i64>()
+        .parse::<i32>()
         .unwrap();
 
     let tlikes = read_to_string( "telegram/".to_string() + &to )
         .unwrap_or("0\n".to_string())
         .lines()
         .nth(0).unwrap()
-        .parse::<i64>()
-        .unwrap() + 1;
+        .parse::<i32>()
+        .unwrap() + amt;
 
     info!("update likes in filesystem {:?} {:?} {:?}",
         to, tlikes,
         write("telegram/".to_string() + &to, tlikes.to_string()));
 
-    let text = format!("{}({})+liked+{}({})", froms, flikes, tos, tlikes);
+    let text = format!("{}{}{}", froms, num2heart(tlikes), tos);
     info!("{:?} -> msg telegram {:?}", text, sendmsg(botkey, &chat_id, &text).await);
 }
 
-async fn do_all(req: HttpRequest, body: web::Bytes) -> HttpResponse {
-    info!("args {:?}", std::env::args());
-    let botkey = &std::env::args().nth(1).unwrap();
-    let chat_id = &std::env::args().nth(2).unwrap();
+async fn do_all(args: web::Data<Args>, req: HttpRequest, body: web::Bytes) -> HttpResponse {
+    info!("args {:?}", args);
+    let botkey = &args().nth(1).unwrap();
+    let chat_id = &args().nth(2).unwrap();
     let json = match body2json(&body) {
         Ok(json) => json,
         Err(e) => {
@@ -282,24 +321,22 @@ async fn do_all(req: HttpRequest, body: web::Bytes) -> HttpResponse {
     HttpResponse::from("")
 }
 
-fn ginfo<T: std::fmt::Debug>(e: T) { info!("{:?}", e); }
-fn gerror<T: std::fmt::Debug>(e: T) { error!("{:?}", e); }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()>{
-    ::pretty_env_logger::try_init();
-
-    println!("{:?}", std::env::args());
-    info!("{}:{} ::{}::async-main()", std::file!(), core::line!(), core::module_path!());
+    ::pretty_env_logger::try_init().unwrap();
+    info!("{}:{} ::{}::async-main() {:?}", std::file!(), core::line!(), core::module_path!(), std::env::args());
 
     let mut ssl_acceptor_builder = SslAcceptor::mozilla_intermediate(SslMethod::tls())?;
     ssl_acceptor_builder .set_private_key_file("key.pem", SslFiletype::PEM)?;
     ssl_acceptor_builder.set_certificate_chain_file("cert.pem")?;
 
-    HttpServer::new( || App::new()
-            .service(
-                web::resource("*")
-                .route( Route::new().to(do_all) ) ) )
+    HttpServer::new(
+        || App::new()
+        .data(std::env::args())
+        .service(
+            web::resource("*")
+            .route( Route::new().to(do_all) ) ) )
     .bind_openssl("0.0.0.0:8443", ssl_acceptor_builder)?
     .run()
     .await
